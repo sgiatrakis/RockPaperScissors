9e2d1c2f1f632ade40561e0eb35c6363d927d0f5
Lysimachos Giatrakis <sgiatrakis@gmail.com>
2022-01-04 20:14:59 +0200

    Xcode Project init

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Lysimachos Giatrakis on 4/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
